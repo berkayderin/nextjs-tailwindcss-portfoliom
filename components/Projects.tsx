ddc6ee8776c581c5e1630ef3e59308242d598dfe
Berkay Derin <zewodi@gmail.com>
2024-05-14 23:49:42 +0300

    Projects add.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -272,7 +272,7 @@ const Projects = () => {
 				{/* project card 8*/}
 				<div className="flex flex-col md:flex-row justify-center items-center space-x-0 space-y-5 md:space-y-0 md:space-x-1 w-full md:w-[75rem] py-5">
 					<div className="flex flex-col justify-center items-start space-y-3 w-full px-5">
-						<h3 className="text-4xl font-extrabold">06</h3>
+						<h3 className="text-4xl font-extrabold">08</h3>
 						<h3 className="text-2xl font-bold">Nextjs Landing Page</h3>
 						<p className="text-lg font-normal text-justify text-zinc-500">
 							Nextjs ve tailwindcss kullanarak geliştirdiğim bir landing page projesidir. Responsive tasarım
@@ -306,6 +306,47 @@ const Projects = () => {
 						width={530}
 						height={398}
 					/>
+				</div>
+				{/* project card 9*/}
+				<div className="flex flex-col md:flex-row justify-center items-center space-x-0 space-y-5 md:space-y-0 md:space-x-1 w-full md:w-[75rem] py-5">
+					<Image
+						src="/img/project.png"
+						alt=""
+						className="w-full md:w-[530px] h-auto md:h-[398px]"
+						width={530}
+						height={398}
+					/>
+					<div className="flex flex-col justify-center items-start space-y-3 w-full px-5">
+						<h3 className="text-4xl font-extrabold">09</h3>
+						<h3 className="text-2xl font-bold">FullStack Nextjs Netflix App</h3>
+						<p className="text-lg font-normal text-justify text-zinc-500">
+							Nextjs 14, NextAuth, Prisma, Mongodb, Zustand, Swiper Slider, Tailwindcss kullanılarak geliştirilmiş bir
+							Netflix Clone uygulamasıdır. Kullanıcılar, filmleri inceleyebilir, favori listelerine ekleyebilir ve
+							filmleri izleyebilir. NextAuth ile güvenli kullanıcı doğrulaması sağlanmıştır. Prisma ORM ile veritabanı
+							işlemleri gerçekleştirilmiştir. Zustand ile global state yönetimi sağlanmıştır. Swiper Slider ile slider
+							özelliği eklenmiştir. Tailwindcss ile responsive tasarım kodlanmıştır.A
+						</p>
+						<div className="flex flex-row justify-start items-center space-x-5">
+							<a
+								href="https://fullstack-nextjs-netflix-app.vercel.app/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="flex flex-row justify-center items-center space-x-2 bg-white text-black px-5 py-2 rounded-md transition-all hover:bg-gray-200"
+							>
+								<FiExternalLink />
+								<span>Projeyi Görüntüle</span>
+							</a>
+							<a
+								href="https://github.com/berkayderin/Fullstack-Nextjs-Netflix-App"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="flex flex-row justify-center items-center space-x-2 bg-white text-black px-5 py-2 rounded-md transition-all hover:bg-gray-200"
+							>
+								<FaGithub />
+								<span>GitHub</span>
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
